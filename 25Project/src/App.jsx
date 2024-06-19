28ce33a543f2a26c1cfd16dcee6ba4eba3d98c7e
hmsazzad <hmsazzad980@gmail.com>
2024-06-20 00:30:43 +0600

    image-slider

--- 25Project/src/App.jsx
+++ 25Project/src/App.jsx
@@ -1,11 +1,15 @@
 import Accordian from "./components/accordian";
+import ImageSlider from "./components/image_slider";
 import RandomColor from "./components/random_color";
 import StarRating from "./components/star_rating";
 
 export default function App() {
   return <div>
     {/*<Accordian/>
-    <RandomColor/>*/}
-    <StarRating noOfStars={10}></StarRating>
+    <RandomColor/>
+    <StarRating noOfStars={10}></StarRating>*/}
+    <ImageSlider url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}/>
   </div>
 }
